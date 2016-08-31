06972ce0dd9b0fbee1943e43f2f4efdd43126f0b
n8sters <ncpowers93@gmail.com>
2016-08-31 15:49:04 -0700

    does what you think
    so far so good. Text may be edited

---
+++ Radius.py
@@ -0,0 +1,12 @@
+#This program calculates the circumference of a circle
+
+print("What is the radius of your circle?")
+
+radius = input()
+
+import math
+
+circ = 2 * math.pi * radius
+
+print "The circumference of a circe with radius " + str(radius) + " is " + str(circ)
+
